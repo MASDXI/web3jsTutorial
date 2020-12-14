51f9faa2227098803e5219e727a249cce29f3ba6
Sirawit Teachvanitch <31649128+MASDXI@users.noreply.github.com>
2020-12-14 13:32:46 +0700

    Create skeleton.sol

---
+++ contract/skeleton.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: none
+pragma solidity >= 0.5.0 < 0.8.0;
+// not recommending to using in production
+pragma experimental ABIEncoderV2;
+
+/**
+ * @author 
+ * @title 
+ * @dev 
+ */
+contract skeleton {
+    
+    
+}
